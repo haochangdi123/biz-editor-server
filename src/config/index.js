bac4cd52e9acde9999b6537cc908ecbb5a0ac41e
changdi.hao <changdi.hao@picahealth.com>
2022-09-14 16:20:10 +0800

    feat：根据环境不同，配置不同的跨域设置

--- src/config/index.js
+++ src/config/index.js
@@ -1,24 +1,14 @@
 /**
- * 连接数据库
+ * @description 配置项
  */
-module.exports = {
-    // 连接 mysql 数据库
-    mysqlConf: {
-        host: 'localhost',
-        user: 'root',
-        password: 'hao123chang123',
-        port: '3306',
-        database: 'imooc_lego_course',
-    },
-    // 连接 mongodb 数据库
-    mongodbConf: {
-        host: 'localhost',
-        port: '27017',
-        dbName: 'imooc_lego_course',
-    },
-    // 连接 redis 数据库
-    redisConf: {
-        host: '127.0.0.1',
-        port: '6379',
-    },
-}
+
+const { isPrd, isPrdDev } = require('../utils/env')
+
+// 获取各个环境的不同配置文件
+let fileName = 'dev.js'
+if (isPrdDev) fileName = 'prd-dev.js'
+if (isPrd) fileName = 'prd.js'
+
+ const conf = require(`./envs/${fileName}`) // eslint-disable-line
+
+module.exports = conf
